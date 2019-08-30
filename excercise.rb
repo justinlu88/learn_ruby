6992e650f6916e08c63b209d14fb6e190b4e93c6
Justin Lu <lujustin130@gmail.com>
2019-08-30 14:31:33 +0100

    maximum

--- excercise.rb
+++ excercise.rb
@@ -2,4 +2,13 @@ def amultipleminus7(a,b)
   return (a*b)-7
 end
 
-p amultipleminus7(1,2)
+# p amultipleminus7(1,2)
+
+def maximum(a,b)
+  if (a > b)
+    return a
+  else
+    return b
+  end
+end
+p maximum(3,1100)
